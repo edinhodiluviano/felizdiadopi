b877aba969d872884579f4ffdc3df0e8206f4557
Edinho <git@final04.com>
2021-03-14 00:14:06 -0300

    added save data

--- api/main.py
+++ api/main.py
@@ -1,5 +1,9 @@
+import os
+import datetime as dt
+from pathlib import Path
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -21,3 +25,28 @@ class Result(BaseModel):
 @app.post("/input")
 def input(result: Result):
     return {}
+
+
+class Config:
+    data = Path("./data")
+
+
+class ResultSave(Result):
+    timestamp: str = Field(default_factory=dt.datetime.utcnow)
+
+    def line(self):
+        return self.json() + "\n"
+
+
+def save(result: Result):
+    result_save = ResultSave(**result.dict())
+    line = result_save.line()
+    file = filename()
+    with open(file, "a") as f:
+        f.write(line)
+    return file
+
+
+def filename():
+    pid = os.getpid()
+    return Config().data / f"{pid}.jsonl"
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,8 @@
+import json
+import datetime as dt
+from pathlib import Path
+from unittest.mock import patch
+
 import pytest
 from fastapi.testclient import TestClient
 
@@ -38,3 +43,55 @@ def test_input_returns_200_when_post_with_good_input(client):
     data = dict(size=100_000_000, inside=70_000_000)
     resp = client.post("/input", json=data)
     assert resp.status_code == 200
+
+
+@pytest.fixture(autouse=True, scope="function")
+def config(tmpdir):
+    class TestConfig:
+        data = Path(tmpdir)
+
+    config_instance = TestConfig()
+    with patch("api.main.Config", return_value=config_instance):
+        yield config_instance
+
+
+def test_config(config):
+    assert len(str(config.data)) > 15
+    assert len(list(config.data.glob("*"))) == 0
+
+
+def test_save_results(config):
+    result = main.Result(size=10_000_000, inside=6_000_000)
+    file = main.save(result)
+    files = list(config.data.glob("*"))
+    assert len(files) == 1
+    assert files[0] == file
+
+
+def test_save_only_one_file(config):
+    for size in range(1_000_000, 11_000_000, 1_000_000):
+        result = main.Result(size=size, inside=6_000_000)
+        main.save(result)
+    files = list(config.data.glob("*"))
+    assert len(files) == 1
+    # basically a new test here... just saving some io time
+    with open(files[0]) as f:
+        lines = f.read()
+    lines = lines.splitlines()
+    assert len(lines) == 10
+
+
+def test_save_format(config):
+    result = main.Result(size=10_000_000, inside=6_000_000)
+    file = main.save(result)
+    with open(file) as f:
+        lines = f.read()
+    lines = lines.splitlines()
+    d = json.loads(lines[0])
+    assert "size" in d
+    assert "inside" in d
+    assert "timestamp" in d
+    d = dt.datetime.fromisoformat(d["timestamp"])
+    assert 1990 < d.year < 2050
+    delta = dt.datetime.utcnow() - d
+    assert 0 < delta.total_seconds() < 1
